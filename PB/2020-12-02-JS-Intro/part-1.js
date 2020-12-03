5acf7c0b169a13dfbabb029e9aa8a6bd2f430a6e
marko <marko.pacak@digitalcareerinstitute.org>
2020-12-03 10:19:24 +0100

    added pb lesson

---
+++ PB/2020-12-02-JS-Intro/part-1.js
@@ -0,0 +1,21 @@
+// No difference
+var num1 = 4;
+let num2 = 6;
+
+console.log(num1 + num2);
+
+console.log("num1" + "num2")
+
+
+const num3 = 5;
+// num3 = 6; // not allowed because it's a constant
+
+num1 = 6; // assigning a value to a previously declared variable
+
+console.log(num1 + num2);
+
+// naming conventions
+// https://www.w3schools.com/js/js_conventions.asp
+var myVariable = 5; // camel case is better!
+
+var my_variable = 5; // snake case no good
---
+++ PB/2020-12-02-JS-Intro/part-2.js
@@ -0,0 +1,87 @@
+// Primitive Data Types
+
+// Number
+console.log("------------------ Part 1: Numbers")
+
+let var1 = 4;       // integer
+console.log(var1);
+var1++;
+console.log(var1);
+var1--;
+console.log(var1);
+
+let var2 = 3.14;    // float / double
+
+console.log(var1 + var2);
+
+let var3 = 5_000_000; // 5'000'000 = 5.000.000 = 5 millions
+let var4 = 5000000; 
+
+console.log(var3 - var4);
+
+let var5 = var4 - var1 * 250;
+console.log(var5);
+
+var5 = (var4 - var1) * 250;
+console.log(var5);
+console.log("" + var5);
+
+// Strings
+console.log("------------------ Part 2: Strings")
+var c1 = "Hello, "
+var c2 = "World!"
+console.log(c1 + c2);
+
+console.log(c1 + c2 + " " + var5);
+
+// Strings and Numbers
+console.log("------------------ Part 3: Strings and Numbers")
+var n1 = 5;
+var n2 = '3';
+
+console.log(n1 + n2);
+console.log(n1 + parseInt(n2));
+
+var n3 = parseInt("n3");
+console.log(n3); // prints NaN = Not A Number
+
+// typeof
+console.log("------------------ Part 4: typeof")
+console.log(typeof n1);
+console.log(typeof n2);
+console.log(typeof n3);
+
+
+// Booleans
+console.log("------------------ Part 5: Booleans")
+
+/*
+This is a multi-line comment
+*/
+
+var b1 = true;
+var b2 = false;
+
+var nn1 = 4;
+var nn2 = 4;
+
+var b3 = (nn1 == nn2);
+
+console.log(b3);
+
+var s1 = "Hello"
+var s2 = "Hello!";
+
+console.log(s1 == s2);
+
+// Nulls
+
+var nn3 = 6;
+nn3 = null;
+
+console.log(nn3 + 7);
+
+var ss1 = "Hola"
+var ss2 = null;
+
+console.log(ss1 + ss2);
