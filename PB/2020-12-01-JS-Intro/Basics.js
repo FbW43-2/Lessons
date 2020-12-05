9ac89972d2167212d28ff545770688324835e06b
Mohammadreza <mohammad.r.ashouri@gmail.com>
2020-12-05 16:28:32 +0100

    adding the Java basic practices with variables strict comments ...

---
+++ PB/2020-12-01-JS-Intro/Basics.js
@@ -0,0 +1,73 @@
+"use strict";
+
+
+/*
+Long comments
+
+*/
+
+
+
+var AccountMoney = 100 ;
+var UnknownVarible;
+var AccountHolder = "Mo";
+var AccountIBAN = 'DE12-1234-1234-1234';
+var Hobbies = "Sport \n Movies";
+let BranchName="Berlin Mitte";
+
+
+
+console.log(typeof AccountMoney);
+
+ PI=3.145;
+//console.log(PI * 3 * 3);
+
+AccountMoney = 500 ;
+//console.log(BranchName);
+var Drivinglicense;
+
+Drivinglicense=1234567890;
+
+let drivingLicense;
+drivingLicense="Driving License";
+
+
+//let Drivinglicense;   //Hoisting we need to change this to var ; let --> error
+
+//X=343;
+//console.log(X);
+
+
+let apple=2;
+let applE=4;
+var appLe=8;
+
+let 我 = '...';
+
+//console.log(apple,applE,appLe, 我);
+
+
+console.log(Y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
