291c42cb48851df6b745dc37dbfe5b8362b00dcd
Mohammadreza <mohammad.r.ashouri@gmail.com>
2020-12-07 14:52:36 +0100

    BookStore App

---
+++ PB/2020-12-07/BookStore.js
@@ -0,0 +1,71 @@
+'use strict';
+
+
+
+
+let Cars = [
+    1,
+     555,
+     64565465464
+    ];
+console.log (Cars[2]);
+
+// An object must have properties
+
+
+// Books is an array of objects
+let Books =  [
+
+{BookTitle :"War of Art", Author : "Steven Pressfield", Price : 35.75, Quantity : 10000},
+{BookTitle :"Learning HTML in 21 Days!", Author : "Mohammadreza Ashouri", Price : 10, Quantity : 10000},
+{BookTitle :"Hack Your Network!", Author : "Bill Gates", Price : 12, Quantity : 4000},
+{BookTitle :"C++ Programming", Author : "Eugen", Price : 15, Quantity : 10000},
+{BookTitle :"How to Promote Your Songs", Author : "Heriberto", Price : 99, Quantity : 10000},
+{BookTitle :"Develope Your Own Web App in PHP", Author : "Raul", Price : 35.75, Quantity : 10000},
+{BookTitle :"Data Structures in Java", Author : "Elun Musk", Price : 100, Quantity : 40000}
+
+];
+
+// For - Loop
+
+
+
+
+
+let StrMessage;
+
+
+/*
+
+for(let Index=0; Index < Books.length ; Index ++) {
+
+    StrMessage = ` The Book Number ${Index+1}  -- > Book Title is \"${Books[Index].BookTitle}\", Price is $ ${Books[Index].Price}`;
+    console.log(StrMessage);
+
+}
+*/
+
+
+
+/*
+console.log(Books[0].BookTitle);
+console.log("$"+Books[0].Price);
+
+Books[0].Price=25;
+
+console.log("$"+Books[0].Price);
+
+*/
+
+
+/*
+var cars = [
+    1,
+    "Volvo",
+    "BMW"
+  ];
+  */
+
+
+
+
