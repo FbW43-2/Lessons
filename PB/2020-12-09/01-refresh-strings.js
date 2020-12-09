ed37dadba1c489b37a3219eca17ff38cd6a310dc
Marko Pacak HP <info@markopacak.com>
2020-12-09 09:30:46 +0100

    add numbers and strings refresh

---
+++ PB/2020-12-09/01-refresh-strings.js
@@ -0,0 +1,15 @@
+var s = "HeLloOoOo";
+console.log(`Step 1: ${s}`);
+
+s = s.toLowerCase();
+console.log(`Step 2: ${s}`);
+
+s = s.toUpperCase();
+console.log(`Step 3: ${s}`);
+
+console.log(`Step 4: ${s.includes("ELL")}`);
+
+var s2 = "   HELLO O O O    O    O   "
+console.log(`Step 5: ${s2}`);
+
+console.log(`Step 6: ${s2.trim()}`)
---
+++ PB/2020-12-09/02-refresh-numbers.js
@@ -0,0 +1,5 @@
+var s = "Hello my age is: ";
+var n = 29;
+
+console.log(s + n);
+console.log(`${s}${n}`)
