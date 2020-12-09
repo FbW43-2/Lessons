15dfa9f683d979429d3c7fd9703335c742eec428
Marko Pacak HP <marko.pacak@digitalcareerinstitute.org>
2020-12-09 13:55:37 +0100

    added js overview

--- PB/2020-12-09/08-if-statements.js
+++ PB/2020-12-09/08-if-statements.js
@@ -1,17 +1,15 @@
 
-var person1 = {
+var people = [{
     first_name: "Luke",
     last_name: "Hermann",
     has_beard: true,
     job_status: {
-        is_student: false,
+        is_student: true,
         profession: "Woodcutter"
     },
     age: 31,
-    test_results: [7, 3, 4]
-}
-
-var person2 = {
+    test_results: [7, 3, 5]
+}, {
     first_name: "Bobby",
     last_name: "Solo",
     has_beard: true,
@@ -21,9 +19,7 @@ var person2 = {
     },
     age: 17,
     test_results: [8, 9, 5]
-}
-
-var person3 = {
+}, {
     first_name: "Giulia",
     last_name: "Giulietti",
     has_beard: false,
@@ -33,20 +29,15 @@ var person3 = {
     },
     age: 24,
     test_results: [4, 3, 2]
-}
+}]
 
 
-
-
-
-
-var person = person1;
 /* 
 Algorithm 1
 ==================================
 
 If the person job is woodcutter, and has had at least a grade 5* in school (no matter if it's still studying or not),
-then tell us wheteher or not they have a beard, and also tell us their best test result (only if they are still studying).
+then tell us wheteher or not they have a beard.
 
 If not, then just print the person last name.
 
@@ -55,19 +46,7 @@ If not, then just print the person last name.
 */
 
 
-if (person.job_status.profession.toLowerCase() === "woodcutter" && person.test_results.indexOf(5) >= 0) {
-    console.log(person.has_beard);
-
-    if (person.job_status.is_student) {
-        console.log(Math.max(person.test_results));
-    }
-} else {
-    console.log(person.last_name);
-}
-
-// === 
-
-if (person.job_status.profession.toLowerCase() === "woodcutter") {
+if ((person.job_status.profession !== null && person.job_status.profession.toLowerCase() === "woodcutter")) {
     if (person.test_results.indexOf(5) >= 0) {
         console.log(person.has_beard);
 
@@ -77,4 +56,46 @@ if (person.job_status.profession.toLowerCase() === "woodcutter") {
     }
 } else {
     console.log(person.last_name);
+}
+
+/* 
+Algorithm 2
+==================================
+
+If the person's job is woodcutter or he's a student that has at least a grade 5 in the last two tests,
+make sure he has not beard.
+If he doesn't then print his last grade and age (if the age is less than 43)
+Otherwise, print his first name, and if his last name is longer than 6 chars, then also print his last name.
+
+If he's no woodcutter and the grade condition is not respected, then just print whether he/she has a beard or not.
+
+* grades go from 0 to 10s
+*/
+
+if (person.job_status.profession.toLowerCase() === "woodcutter" || (person.job_status.is_student && (person.test_results[2] >= 5 || person.test_results[1] >= 5) ) ) {
+    if ( ! person.has_beard) {
+        console.log(person.test_results[2]);
+
+        if (person.age < 43) 
+            console.log(person.age);
+
+    } else {
+        // option 1 -------------------
+        var nameToPrint = person.first_name;
+
+        if (nameToPrint.last_name.length > 6)
+            nameToPrint += " " + person.last_name;
+
+        console.log(nameToPrint);
+
+        // option 2 -------------------
+        if (person.last_name.length <= 6)
+            console.log(person.first_name)
+        else
+            console.log(person.first_name + " " + person.last_name);
+
+    }
+
+} else {
+    console.log(person.has_beard);
 }
---
+++ PB/2020-12-09/09-js-overview.js
@@ -0,0 +1,36 @@
+// arrays: square brackets
+
+var a1 = [1, 2, 3, 4];
+
+// objects: curly braces
+
+var o1 = { 
+    property_name: "property_value",    // this is a property of an object
+    address: "Sonnenallee 11111"        // this is also a property of an object
+};
+
+
+Math.min(1, 2, 3, 4); // min is the function name       1, 2, 3, 4 are parameters
+
+
+// these are both variables
+var snakeName = "Snakky";
+let snakeAge = 1;
+
+const PI = 3.14 // this is a constant, and I'm writing it uppercase as per convention
+
+
+// this is a function definition
+function sum(x, y) {   // x and y are function parameters
+    return x + y; // here I'm returning a value from my function
+}
+
+// here I'm calling the function
+var s = sum(4, 5);
+
+// code blocks also have curly braces
+if (s >= 14) { // with this curly brace here I start a code block
+    // ...
+    // ...
+    // ...
+} // and here I finish it
