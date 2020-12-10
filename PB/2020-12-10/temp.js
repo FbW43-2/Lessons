9019c206fe289e7157856238f700bdc3a5f772b0
Marko Pacak HP <marko.pacak@digitalcareerinstitute.org>
2020-12-10 14:54:19 +0100

    Add some exercises solutions

---
+++ PB/2020-12-10/temp.js
@@ -0,0 +1,29 @@
+
+
+// Exercise 1 & 2
+var x = 6;
+var y = 2231;
+var z = 72;
+
+
+if ( (x >= 50 && x <= 99) || (y >= 50 && y <= 99) || (z >= 50 || z <= 99) ) {
+    // print true
+}
+
+
+// Exercise 3
+var a = 9;
+var b = 11;
+var c = 7;
+
+// Mariana's way
+if (a > b && a > c) {
+    // a is the largest
+    console.log("a");
+} else if (b > c) {
+    // b is the largest
+    console.log("b");
+} else {
+    // c is the largest
+    console.log("c");
+}
