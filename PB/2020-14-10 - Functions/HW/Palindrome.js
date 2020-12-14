a487039590cf4415e28230919fe720b06b1c83d0
Mohammadreza <mohammad.r.ashouri@gmail.com>
2020-12-14 15:51:37 +0100

    a homework

---
+++ PB/2020-14-10 - Functions/HW/Palindrome.js
@@ -0,0 +1,14 @@
+/*
+
+# Palindrome Challenge
+
+What is Palindrome?
+
+"A palindrome is a word, number, phrase, or other sequence of characters which reads the same backward as forward, such as madam, racecar."-- Wikipedia
+
+PROBLEM: 
+1- Write a function that controls whether its argument is a palindrome (return true) or not (return false).
+2- Rewrite the function with function expression.
+3- Instead of the argument variable, use rewrite your function (and expression) with a global variable.
+
+*/
