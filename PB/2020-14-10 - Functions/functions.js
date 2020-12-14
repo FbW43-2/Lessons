4e96a9367509dd40a800d39539e7e435a2bec6da
Mohammadreza <mohammad.r.ashouri@gmail.com>
2020-12-14 10:37:15 +0100

    function declarations in JS

---
+++ PB/2020-14-10 - Functions/functions.js
@@ -0,0 +1,53 @@
+
+'use strict';
+
+
+function Message(strMsg){ // argument, function input
+
+    console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+    console.log(`+++++++++++++++++   ${strMsg}   +++++++++++++++++++++++`);
+    console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+  
+}
+
+
+function CharCount(strInput) 
+{
+
+    if(strInput==undefined){
+      console.log("Warning: Please give me an input string!");
+      return 0;
+    }else
+      return  strInput.length;
+    
+}
+
+
+//----------------------------------- end of function declarations  -------------------
+
+
+
+
+
+
+Message("Welcome to Apple"); // reference , callsite, function call, invoke  , parameter
+
+/*
+some statements;
+some statements;
+some statements;
+some statements;
+*/
+
+
+
+
+let City="Berlin";
+
+//CharCount();  // it return 0
+
+let inputStrLen=CharCount(City); // assign a function return value to a variable!
+
+console.log(City+ " has " + inputStrLen + " characters!");
+
+Message(" Goodbye Apple User")
