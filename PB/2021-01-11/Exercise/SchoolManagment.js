75237ee7756a794a969b25f6aa4a9431f6f60d31
Mohammadreza <mohammad.r.ashouri@gmail.com>
2021-01-11 14:23:20 +0100

    SchoolManagement Exercise

---
+++ PB/2021-01-11/Exercise/SchoolManagment.js
@@ -0,0 +1,17 @@
+/*
+
+Exercise [Java Classes and Objects]
+
+Assume you are asked to create a school management system.
+
+1- Analyze of potential objects in a school management system (e.g., student, teacher,...) 
+2- Create re-usable classes for potential objects 
+3- Implement essential properties and methods for each object
+4- Create constructors for your classes
+
+Feel free to brainstorm!
+
+[optional] 5- Try to get properties values from HTML forms (e.g., for student registration method, etc.)
+
+*/
+
