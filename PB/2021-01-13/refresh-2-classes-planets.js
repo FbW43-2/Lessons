e25e8a8a8509a971253db536afa447bad662522e
Marko Pacak HP <marko.pacak@digitalcareerinstitute.org>
2021-01-13 11:45:46 +0100

    added base class spaceobject

--- PB/2021-01-13/refresh-2-classes-planets.js
+++ PB/2021-01-13/refresh-2-classes-planets.js
@@ -1,20 +1,34 @@
-/*
-Create a planet class like this one 
-Make sure it has the properties: name, diameter (in km), color
-Create 8 instances of planets (use the properties of our solar system)
-When you create a planet, print it's properties (in the constructor) 
-*/
-class Planet {
-    constructor(planetName) {
-        this.name = planetName;
+
+class SpaceObject {
+    constructor(objectName) {
+        this.name = this.checkValidName(objectName);
+    }
+
+    checkValidName(name) {
+        if (name === null || name === undefined)
+            throw Error("No name set");
+    
+        if (name.length < 2)
+            throw Error("The name must be at least 2 char long")
+    
+        // add other constraint check here...
+    
+        return name 
     }
 }
 
-class Star {
+
+class Planet extends SpaceObject {
+    constructor(planetName) {
+        super(planetName);
+    }
+}
+
+class Star extends SpaceObject {
     planetarySystem = null;
 
     constructor(starName) {
-        this.name = starName;
+        super(starName);
     }
 
     updateSystem(system) {
@@ -26,12 +40,12 @@ class Star {
     }
 }
 
-class PlanetarySystem {
+class PlanetarySystem extends SpaceObject {
     /*
     A planetary system has at least one star and multiple planets
     */
-    constructor(name, star) {
-        this.name = name;
+    constructor(systemName, star) {
+        super(systemName);
         this.star = star;
         this.star.updateSystem(this);
     }
@@ -40,13 +54,10 @@ class PlanetarySystem {
 // ------ Main program
 
 var star = new Star("Sun");
+var planet1 = new Planet("Mercury");
+var planet2 = new Planet("G");
 
 var system1 = new PlanetarySystem("Solar System", star);
 console.log(`The sun's planetary system is ${star.planetarySystem.name}`);
 
-var system2 = new PlanetarySystem("Palomar I", star);
-console.log(`The sun's planetary system is ${star.planetarySystem.name}`);
-
-
-// Here, we want our program to crash / throw an error, because two solar systems cannot share the same sun
-// How do we plan to do this? We make sure that the star is linked to only one planetary system
+var system2 = new PlanetarySystem("Palomar I", new Star("Orion"));
