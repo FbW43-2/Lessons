018ba385beee8d72e754c6d530b6e5271a28f622
Mohammadreza <mohammad.r.ashouri@gmail.com>
2021-01-19 14:29:11 +0100

    Queue Exercise...

--- PB/2021-01-19/Queue_Exercise.js
+++ PB/2021-01-19/Queue_Exercise.js
@@ -4,6 +4,6 @@ Create a queue with Class in Javascript with the following methods:
 
 1-  View the first element at the front of the queue
 2-  View the queue by For-Loop or While-Loop
-3-  Replace all existing elements of the queue
+3-  Replace all existing elements of the queue with the method argument
 
 */
