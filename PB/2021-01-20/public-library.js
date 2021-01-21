d18f567814ac78cadf4fd8cb78b63d5cb40ddc9a
Marko Pacak HP <marko.pacak@digitalcareerinstitute.org>
2021-01-21 09:59:13 +0100

    continued the library exercise

--- PB/2021-01-20/public-library.js
+++ PB/2021-01-20/public-library.js
@@ -24,6 +24,47 @@ class Library {
         }
     }
 
+    /**
+     * Returns an array of books.
+     * If there no books matching the query, then it
+     * returns an empty array.
+     */
+    getBooksOfAuthor(author) {
+        if (author instanceof String || typeof author === 'string')
+            return this.getBooksOfAuthorLastName(author);
+
+        if (!(author instanceof Author))
+            throw new Error(`Parameter must be of type Author`);
+
+        var returnValue = [];
+
+        for (var shelf of this.shelves) {
+            for (var book of shelf.books) {
+                
+                if (book.author === author) {
+                    returnValue.push(book);
+                }
+            }
+        }
+
+        return returnValue;
+    }
+
+    getBooksOfAuthorLastName(authorLastName) {
+        var returnValue = [];
+
+        for (var shelf of this.shelves) {
+            for (var book of shelf.books) {
+                
+                if (book.author.lastName === authorLastName) {
+                    returnValue.push(book);
+                }
+            }
+        }
+
+        return returnValue;
+    }
+
 }
 
 class Shelf {
@@ -89,13 +130,44 @@ class Shelf {
             console.log("The two shelves belong to different libraries");
             return;
         }
-
+        
         // remove the book from the books array
         var indexToRemove = this.books.indexOf(book);
         this.books.splice(indexToRemove, 1);
 
         // add the book to the other shelf
         otherShelf.addBook(book);
+    }
+
+    /**
+     * In the future, when we see 'passing filter functions' as parameters,
+     * you would have one single method for all cases... E.g:
+     * 
+     * moveToOtherShelf(otherShelf, condition)
+     */
+    moveToOtherShelfIfMoreThanXPages(otherShelf, pages) {
+        // we go through all of this, instead of simply moving 
+        // books from one shelf to the other, becasue we learned
+        // that we should not delete elements from an array that we are iterating over.
+
+        // create an empty temporary array where to store the books that we will later move
+        var booksToMove = [];
+        
+        // determine what books need to be moved
+        for (var book of this.books) {
+            console.log(`Iterating book ${book.author}`);
+            if (book.pages > pages) {
+                // if the book has more than X pages, place it in the array
+                // so it will be later moved
+                booksToMove.push(book);
+            }
+        }
+
+        // finally iterate over the temp array, and move the books
+        // using the proper function
+        for (var book of booksToMove) {
+            this.moveToOtherShelf(book, otherShelf);
+        }
     }
 
     hasBook(book) {
@@ -153,15 +225,23 @@ shelf1.addBook(book1);
 shelf1.addBook(book11);
 shelf1.addBook(book3);
 
-shelf1.displayBooks();
-shelf2.displayBooks();
+// shelf1.displayBooks();
+// shelf2.displayBooks();
 
-shelf1.moveToOtherShelf(book11, shelf3);
+// shelf1.moveToOtherShelf(book11, shelf3);
 
-shelf1.displayBooks();
-shelf2.displayBooks();
+// shelf1.displayBooks();
+// shelf2.displayBooks();
 
-library1.burnBooksPublishedBefore(2005);
-console.log("After having burned the books");
+// library1.burnBooksPublishedBefore(2005);
+// console.log("After having burned the books");
+// shelf1.displayBooks();
+// shelf2.displayBooks();
+
+// console.log(library1.getBooksOfAuthor(author1));
+// console.log(library1.getBooksOfAuthor("John"));
+
+shelf1.moveToOtherShelfIfMoreThanXPages(shelf2, 14);
+
 shelf1.displayBooks();
 shelf2.displayBooks();
