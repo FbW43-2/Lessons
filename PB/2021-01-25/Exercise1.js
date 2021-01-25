af8ca46c2da5763aa22d01617ffe9faaf32a8a39
Mohammadreza <mohammad.r.ashouri@gmail.com>
2021-01-25 11:57:38 +0100

    linked list exercise 1..

--- PB/2021-01-25/Exercise1.js
+++ PB/2021-01-25/Exercise1.js
@@ -3,7 +3,7 @@
 Implement the following methods/functions in the linkedlist class:
 
 1- Insert a node at the head of a linked list
-2- Print all elements of a a linked list with the "While" loop
+2- Print all elements of a linked list with the "While" loop
 3- Insert a Node at the tail (ending node) of a Linked List
 4- Delete a Node in a linked list
 5- Print all nodes' data in reverse
