75f4c2423674fa55d23284e9c88cc681117f878a
Mohammadreza <mohammad.r.ashouri@gmail.com>
2021-01-26 10:15:11 +0100

    linkelist exercise solution

--- PB/2021-01-25/Exercise1.js
+++ PB/2021-01-25/Exercise1.js
@@ -9,3 +9,169 @@ Implement the following methods/functions in the linkedlist class:
 5- Print all nodes' data in reverse
 
 */
+
+
+class Stack {   // because of reverse printing
+
+    data;
+    top;
+
+
+    constructor(){
+
+        this.data=[];
+        this.top=0;
+    }
+
+    push(element){
+
+        this.data[this.top]=element;
+        this.top=this.top+1;
+
+    }
+
+    pop(){
+
+        // the stack shoukd not be empty
+
+       if(this.isEmpty() === false)
+       {
+        this.top=this.top-1; 
+        return this.data.pop();
+
+       } 
+    
+
+    }
+
+    isEmpty(){
+
+        return this.top===0; // true
+    }
+
+
+}
+
+
+
+
+// node  list
+
+class Node {
+
+
+    data;
+    next;
+
+    constructor(_data){
+
+        this.data=_data;
+        this.next=null;
+
+    }
+
+}
+
+
+class Linkedlist {
+
+
+    head;// head node
+  
+
+
+    constructor(){
+
+        this.head=null;
+        
+    }
+
+
+
+    InsertNode(_data){ // insert after the tail node
+
+        let mynode=new Node(_data);
+
+        if(this.head==null){
+            this.head=mynode;
+
+        }else{
+
+
+            let Current=this.head;
+            while(Current.next!=null){
+                  Current=Current.next; //  1 -> 2 -> [3] ->null
+            }
+
+            Current.next=mynode;   // 1-> 2 -> 3 -> [4 new node] - >null;
+
+
+        }
+
+    }
+
+
+
+    PrintAll(){
+
+        // [1] -> 2 -> 3 -> 4 -> null
+
+      let CurrentNode=this.head;
+
+      while(CurrentNode !=null){
+          
+          console.log(CurrentNode.data); // print
+          CurrentNode=CurrentNode.next;
+
+      }
+
+
+    }
+
+
+
+    PrintAllReverse(){
+
+            // 4 -> 3 -> 2 -> 1 
+    
+          let CurrentNode=this.head;// head of linkedlist
+          let myPrintingStack=new Stack(); // create a new stack to hold data
+    
+          while(CurrentNode !=null){
+              
+              myPrintingStack.push(CurrentNode.data); // pushing node.data into our stack
+              CurrentNode=CurrentNode.next;
+    
+          }
+          // myPrinting stack is full in LIFO ==> [4,3,2,1]
+
+        // now let pop the stack to print all data in reverse
+          while(myPrintingStack.isEmpty() == false){
+
+              console.log(myPrintingStack.pop()); 
+          }
+    
+
+          // myPrinting stack is empty by now
+    
+        
+    }
+
+
+
+}
+
+
+//---------------------------
+
+
+let myLinkedlist=new Linkedlist();
+
+myLinkedlist.InsertNode(1);
+myLinkedlist.InsertNode(2);
+myLinkedlist.InsertNode(3);
+myLinkedlist.InsertNode(4);
+
+myLinkedlist.PrintAll();
+console.log('---------------------');
+myLinkedlist.PrintAllReverse();
--- PB/2021-01-25/linkedlist.js
+++ PB/2021-01-25/linkedlist.js
@@ -1,7 +1,7 @@
 
 class LinkedListNode{
 
-      data;
+      data; // could music, video, URL , etc.git
       next; // pointer
 
       constructor(_data){
@@ -11,20 +11,49 @@ class LinkedListNode{
 
       }
 
+}
+
+
+class SinglyLinkedList{
+
+   LinkedListNode head;
+   LinkedListNode tail;
+
+
+   constructor(_data){
+
+         this.head.data=_data;
+         this.head.next=null;
+         this.tail=head;
+         
+   }
+
+   GetHead(){
+       
+         return this.head;
+   }
+
+   GetTail(){
+
+         return this.tail;
+   }
+
+
 
 }
 
 
 //-----------------------
 
+let node0=new LinkedListNode(99); // the first node , i.e., head
 let node1=new LinkedListNode(1);
 let node2=new LinkedListNode(7);
-let node3=new LinkedListNode(21);
+let node3=new LinkedListNode(21); // the last node , i.e., tail
 
-
+node0.next=node1; // new head
 node1.next=node2;
 node2.next=node3;
-// node1 -> node2 -> node3 ->null
+// node1 (head) -> node2 -> node3 (tail) ->null
 node3.next=null;
 
 
