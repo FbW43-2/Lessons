2b660ecdaf8eb7bf703257402111c7e75c14402a
Mohammadreza <mohammad.r.ashouri@gmail.com>
2021-01-26 15:19:38 +0100

    Homework - Linked list...

--- PB/2021-01-26/Homework_Linkedlist.js
+++ PB/2021-01-26/Homework_Linkedlist.js
@@ -2,19 +2,20 @@
 Homework : Linked List:
 
 
-1- Create a small program that holds personal information (e.g., full name, gender, phone, address) inside a link list.
+1- Create a small program that holds personal information (e.g., full name, gender, phone, address) inside a link list (each node contains a piece of personal information, e.g., full name, etc.)
 2- Create a method for the program that searches for a node in a linked list based on full name and displays other information (from succeeding nodes) 
 For example:
 
 Search("Mo") - > prints "Mo, Male, 017600000, Potsdammer Platz str"
 3- Create an update method that updates full name such as following:
 
+Update(olddata, newdata)
 Update("Mo","Ami"); --> this method must update a full name, so after calling this, the linked list changes to "Ami, Male, 017600000, Potsdamer Platz str"
 
 
 
 
-4 - (This is optional) create a method to remove duplicated data nodes from your linked list.
+4 - (This is optional) create a method to remove duplicated data nodes from your linked list (for a typical linked list).
 For instance
 
 If you linked list is  1 -> 2 -> 2 -> 3 ->4 , after calling the method, your linked list must be like :   1 -> 2 -> 3 -> 4
