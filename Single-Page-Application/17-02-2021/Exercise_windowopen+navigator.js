2756ea04358a7508e1d41bf23f3609e7df258c64
Mohammadreza Ashouri <mohammad.r.ashouri@gmail.com>
2021-02-17 10:47:16 +0100

    window.open and navigator exercise

---
+++ Single-Page-Application/17-02-2021/Exercise_windowopen+navigator.js
@@ -0,0 +1,12 @@
+/*
+
+
+Make a webpage that contains a button.
+1- When a visitor clicks on the button a new small window opens
+2- The new window shows a login webpage (aka username and password)
+3- control the credentials (e.g., username==admin, password=123456) when the results were true open a new window including a welcome message (e.g., welcome to your panel!)
+4- if the authentication was false - > shows a webpage including a warning message with the visitor browser information (e.g., the message would be "you are not allowed to enter the user panel, now we collect your browser information
+and report to our IT security department!
+* TIP: the visitor's information can be gained by the properties of the navigator object
+
+*/
