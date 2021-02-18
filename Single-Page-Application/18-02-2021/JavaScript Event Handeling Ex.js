e2ee2c2a4df7aeff388408b290e9c65ccab3866b
Mohammadreza Ashouri <mohammad.r.ashouri@gmail.com>
2021-02-18 11:04:56 +0100

    JavaScript Event Handling Exercises...

---
+++ Single-Page-Application/18-02-2021/JavaScript Event Handeling Ex.js
@@ -0,0 +1,26 @@
+/*
+
+
+Exercise 1:
+
+- Make a simple HTML form with two inputs (sameprice, discount) and add an HTML button to it. Give this button an onClick event handler. 
+When the button is clicked it should multiply gamePrice by discount and present a new window (e.g., a popup window) containing the phrase "$(result) off!". 
+(You will need concatenation to achieve this.)
+You may want to consider using JavaScript's built-in Maths object to round the number you get in your result to a more reasonable one.
+
+
+Exercise 2:
+
+Create a registration form with the following features:
+1- your form must take common user information such as first name, family name, phone number, email, password, etc.
+2- when a user focuses on each input the background color of that input must turn to yellow.
+3- You need to control the first name and family name are shown with capital letters. 
+4- Check the email and phone numbers with regex to make sure they are entered in the right format.
+5- Before a user can send her registration form, you must control the inputs in terms of emptiness.
+
+
+*/
+
+
+
+
