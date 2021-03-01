2190dfe58e7bfa4c6f1b0fedd06d18d4e41cf54f
Ahmad Osman <ahmad@ahmad-osman.com>
2021-03-01 10:27:48 +0100

    shope start

---
+++ Single-Page-Application/01-03-2021/js/script.js
@@ -0,0 +1,9 @@
+// get HTML elements
+const searchBtn = document.getElementById('searchBtn');
+const searchInp = document.getElementById('searchInp');
+
+// add click event listener to serach button
+searchBtn.addEventListener('click', () => {
+    alert("Hi FBW43-2");
+});
+
