072785446346cc1ca9ce552654e2f177fbfe3ec8
Ahmad Osman <ahmad@ahmad-osman.com>
2021-03-01 12:20:26 +0100

    product search done

--- Single-Page-Application/01-03-2021/js/script.js
+++ Single-Page-Application/01-03-2021/js/script.js
@@ -1,6 +1,7 @@
 // get HTML elements
 const searchBtn = document.getElementById('searchBtn');
 const searchInp = document.getElementById('searchInp');
+const resultUl = document.getElementById('resultUl');
 
 // add click event listener to serach button
 searchBtn.addEventListener('click', () => {
@@ -25,6 +26,28 @@ searchBtn.addEventListener('click', () => {
         if(response.status === 200) {
             response.json().then(results => {
                 console.log(results.results[0].hits);
+                // reset the old results on DOM
+                resultUl.innerHTML = "";
+                // get the results as an array 
+                const hits = results.results[0].hits;
+                // loop through the results to render them
+                for(let i = 0 ; i < hits.length ; i++){
+                    // create template html for each result
+                    let htmlProduct = `<li class="list-group-item">
+                    <div class="media align-items-lg-center flex-column flex-lg-row p-3">
+                        <div class="media-body order-2 order-lg-1">
+                            <h5 class="mt-0 font-weight-bold mb-2">${hits[i].name}</h5>
+                            <p class="font-italic text-muted mb-0 small">${hits[i].description}</p>
+                            <div class="d-flex align-items-center justify-content-between mt-1">
+                                <h6 class="font-weight-bold my-2">EURO${hits[i].price}</h6>
+                            </div>
+                        </div><img src="${hits[i].image}" alt="${hits[i].name}" width="200"
+                            class="ml-lg-5 order-1 order-lg-2">
+                    </div> 
+                </li>`;
+                // add the result html to container element [resultUl]
+                resultUl.innerHTML += htmlProduct;
+                }
             })
         }
     })
