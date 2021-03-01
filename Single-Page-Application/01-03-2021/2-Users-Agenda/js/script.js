56d52ec7e1cad0e6364464f7118f66812f4b1bee
Ahmad Osman <ahmad@ahmad-osman.com>
2021-03-01 16:05:38 +0100

    get users data

---
+++ Single-Page-Application/01-03-2021/2-Users-Agenda/js/script.js
@@ -0,0 +1,27 @@
+// fetch('https://jsonplaceholder.typicode.com/users').then(function(response){
+
+// })
+
+const tableBody = document.getElementById('tableBody');
+
+fetch('https://jsonplaceholder.typicode.com/users').then(response => {
+    if(response.status === 200) {
+        response.json().then(data => {
+            console.log(data);
+            data.forEach(element => {
+                const htmlRow = `<tr>
+                <th scope="row">${element.id}</th>
+                <td>${element.name}</td>
+                <td>${element.username}</td>
+                <td>${element.email}</td>
+                <td>${element.address.street} ${element.address.suite}, ${element.address.zipcode}</td>
+                <td>${element.phone}</td>
+                <td>${element.website}</td>
+                <td>${element.company.name}</td>
+                <td><button class="btn btn-success">Agenda</td>
+              </tr>`;
+              tableBody.innerHTML += htmlRow;
+            });
+        })
+    }
+})
