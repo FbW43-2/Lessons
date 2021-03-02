2a2f092bbbf80798b2b35d262de6856b48288688
Ahmad Osman <ahmad@ahmad-osman.com>
2021-03-02 14:17:36 +0100

    face recognition

---
+++ Single-Page-Application/02.03.2021/face-recognition/js/script.js
@@ -0,0 +1,19 @@
+const input = document.getElementById('myImage');
+const canvas = document.getElementById('myCanvas')
+const myContext = canvas.getContext("2d");
+myContext.drawImage(input, 0, 0)
+
+
+async function detect() {
+    const MODEL_URL = '/js/weights'
+
+await faceapi.loadSsdMobilenetv1Model(MODEL_URL)
+await faceapi.loadFaceLandmarkModel(MODEL_URL)
+await faceapi.loadFaceRecognitionModel(MODEL_URL)
+    let fullFaceDescriptions = await faceapi.detectAllFaces(canvas).withFaceLandmarks().withFaceDescriptors();
+    console.log(fullFaceDescriptions);
+    faceapi.draw.drawDetections(canvas, fullFaceDescriptions)
+}
+
+detect()
+
