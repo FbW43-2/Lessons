23092f73904dc668a26d4c23b84ca2809f29971c
Ahmad Osman <ahmad@ahmad-osman.com>
2021-03-04 11:43:51 +0100

    datatable is done

--- Single-Page-Application/02.03.2021/data-table/js/script.js
+++ Single-Page-Application/02.03.2021/data-table/js/script.js
@@ -2,7 +2,31 @@ fetch('https://jsonplaceholder.typicode.com/users').then(response => {
     if(response.status === 200) {
         response.json().then(results => {
             // write the rest of the code
-            
+            console.log(results);
+            const data = {
+                "headings": [
+                    "Name",
+                    "UserName",
+                    "Email",
+                    "Address",
+                    "Phone",
+                    "Website",
+                    "Company"
+                ],
+                "data": results.map(element => [
+                    element.name,
+                    element.username,
+                    element.email,
+                    element.address.street + " ,"+ element.address.suite + " ," + element.address.zipcode + " ," + element.address.city,
+                    element.phone,
+                    element.website,
+                    element.company.name
+                ])
+            }
+            var myTable = document.getElementById('basic');
+            var dataTable = new DataTable(myTable, {data});
         })
     }
 })
+
+
