a630ab108685ffe381a0649018929987c9f34fcf
Ahmad Osman <ahmad@ahmad-osman.com>
2021-03-15 14:35:51 +0100

    hook useState

--- Single-Page-Application/15.03.2021/functional-components/src/components/addtodo.js
+++ Single-Page-Application/15.03.2021/functional-components/src/components/addtodo.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const AddToDo = () => {
+    // hook useState
+    // 1- create intial state object
+    // 2- state array destructuring using useState
+    const initState ={
+        todoValue: ''
+    };
+
+    const [state, setState] = useState(initState);
+
     return(
         <>
-        <input type="text" placeholder="enter To Do" />
+        <input
+        value={state.todoValue} 
+        onChange={(e) => {setState({todoValue: e.target.value})}}
+        type="text" 
+        placeholder="enter To Do" 
+        />
         <button>add</button>
         </>
     )
