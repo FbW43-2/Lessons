d41846bde9aca8301e30d7dfd2babd49428d0d2a
Ahmad Osman <ahmad@ahmad-osman.com>
2021-03-18 14:09:43 +0100

    carousal start

--- Single-Page-Application/18.03.2021/e-shopper/src/index.js
+++ Single-Page-Application/18.03.2021/e-shopper/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PageHeader from './components/pageHeader';
+import Slider from './components/slider';
 
 const App = () => {
     return (
         <>
             <PageHeader />
+            <Slider />
         </>
     );
 };
