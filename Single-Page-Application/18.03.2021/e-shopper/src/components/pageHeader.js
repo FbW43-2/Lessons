ea53c2c3acedda0ea507891267b17683759c3d7a
Ahmad Osman <ahmad@ahmad-osman.com>
2021-03-18 13:03:40 +0100

    e-shopping to react

---
+++ Single-Page-Application/18.03.2021/e-shopper/src/components/pageHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BarTop from './headerBars/barTop';
+
+const PageHeader = () => {
+    return (
+        <header id="header">
+            <BarTop />
+        </header>
+    );
+};
+
+export default PageHeader;
--- Single-Page-Application/18.03.2021/e-shopper/src/index.js
+++ Single-Page-Application/18.03.2021/e-shopper/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PageHeader from './components/pageHeader';
 
 const App = () => {
     return (
-        <div>
-            E-shopper
-        </div>
+        <>
+            <PageHeader />
+        </>
     );
 };
 
