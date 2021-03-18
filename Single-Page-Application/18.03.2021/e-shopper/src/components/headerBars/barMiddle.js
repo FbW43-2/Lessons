f8b739cc46b32fb46b0375727e97f12e9e0cd4c3
Ahmad Osman <ahmad@ahmad-osman.com>
2021-03-18 13:25:26 +0100

    header done e-shopping

---
+++ Single-Page-Application/18.03.2021/e-shopper/src/components/headerBars/barMiddle.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+
+const BarMiddle = () => {
+    return (
+        <div className="header-middle">
+			<div className="container">
+				<div className="row">
+					<div className="col-md-4 clearfix">
+						<div className="logo pull-left">
+							<a href="index.html"><img src="images/home/logo.png" alt="" /></a>
+						</div>
+						<div className="btn-group pull-right clearfix">
+							<div className="btn-group">
+								<button type="button" className="btn btn-default dropdown-toggle usa" data-toggle="dropdown">
+									USA
+									<span className="caret"></span>
+								</button>
+								<ul className="dropdown-menu">
+									<li><a href="">Canada</a></li>
+									<li><a href="">UK</a></li>
+								</ul>
+							</div>
+							
+							<div className="btn-group">
+								<button type="button" className="btn btn-default dropdown-toggle usa" data-toggle="dropdown">
+									DOLLAR
+									<span className="caret"></span>
+								</button>
+								<ul className="dropdown-menu">
+									<li><a href="">Canadian Dollar</a></li>
+									<li><a href="">Pound</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div className="col-md-8 clearfix">
+						<div className="shop-menu clearfix pull-right">
+							<ul className="nav navbar-nav">
+								<li><a href=""><i className="fa fa-user"></i> Account</a></li>
+								<li><a href=""><i className="fa fa-star"></i> Wishlist</a></li>
+								<li><a href="checkout.html"><i className="fa fa-crosshairs"></i> Checkout</a></li>
+								<li><a href="cart.html"><i className="fa fa-shopping-cart"></i> Cart</a></li>
+								<li><a href="login.html"><i className="fa fa-lock"></i> Login</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+    );
+};
+
+export default BarMiddle;
--- Single-Page-Application/18.03.2021/e-shopper/src/components/pageHeader.js
+++ Single-Page-Application/18.03.2021/e-shopper/src/components/pageHeader.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import BarBottom from './headerBars/barBottom';
+import BarMiddle from './headerBars/barMiddle';
 import BarTop from './headerBars/barTop';
 
 const PageHeader = () => {
     return (
         <header id="header">
             <BarTop />
+            <BarMiddle />
+            <BarBottom />
         </header>
     );
 };
