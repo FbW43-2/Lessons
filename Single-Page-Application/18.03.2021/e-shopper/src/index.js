80da2f82a89d176733791f00f63c96e7537ab037
Ahmad Osman <ahmad@ahmad-osman.com>
2021-03-22 10:05:43 +0100

    e shopping home products section

--- Single-Page-Application/18.03.2021/e-shopper/src/index.js
+++ Single-Page-Application/18.03.2021/e-shopper/src/index.js
@@ -5,6 +5,47 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './pages/home';
 
 const App = () => {
+
+    const products = [
+        {
+            id: 123451,
+            title: 'title1',
+            price: 50,
+            imgUrl: '/images/home/product1.jpg'
+        },
+        {
+            id: 123452,
+            title: 'title2',
+            price: 50,
+            imgUrl: '/images/home/product2.jpg'
+        },
+        {
+            id: 123453,
+            title: 'title3',
+            price: 50,
+            imgUrl: '/images/home/product3.jpg'
+        },
+        {
+            id: 123454,
+            title: 'title4',
+            price: 50,
+            imgUrl: '/images/home/product4.jpg'
+        },
+        {
+            id: 123455,
+            title: 'title5',
+            price: 50,
+            imgUrl: '/images/home/product5.jpg'
+        },
+        {
+            id: 123456,
+            title: 'title6',
+            price: 50,
+            imgUrl: '/images/home/product6.jpg'
+        }
+    ]
+
+
     return (
         <Router>
             <PageHeader />
@@ -12,7 +53,7 @@ const App = () => {
 
 
             <Switch>
-                <Route exact path="/"><Home /></Route>
+                <Route exact path="/"><Home products={products} /></Route>
             </Switch>
 
         </ Router >
