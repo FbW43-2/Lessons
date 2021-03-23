5d4e01f05f18541899198a758881ad56ddc9a447
Ahmad Osman <ahmad@ahmad-osman.com>
2021-03-23 15:17:00 +0100

    getusers

---
+++ Single-Page-Application/23.03.2021/routing-api/src/api/index.js
@@ -0,0 +1,17 @@
+export const getUsers = () => {
+    return new Promise((resolve, reject) => {
+        fetch('https://jsonplaceholder.typicode.com/users').then(response => {
+            if (response.status === 200) {
+                response.json().then(data => {
+                    resolve(data);
+                }).catch(error => {
+                    reject(error);
+                })
+            } else {
+                reject(response.status);
+            }
+        }).catch(error => {
+            reject(error);
+        })
+    })
+}
--- Single-Page-Application/23.03.2021/routing-api/src/components/users.js
+++ Single-Page-Application/23.03.2021/routing-api/src/components/users.js
@@ -1,9 +1,25 @@
-import React from 'react';
-
+import React, {useState, useEffect} from 'react';
+import { Link } from 'react-router-dom';
+import { getUsers } from '../api';
 const Users = () => {
+    const [state, setState] = useState({users: []})
+    useEffect(() => {
+        getUsers().then(data => {
+            setState({users: data});
+        })
+    }, [])
     return (
         <div>
-            
+            <ul>
+                {state.users.map(element => 
+                    <li>
+                        <Link 
+                        to={`/users/${element.id}`}>
+                            {element.name}
+                        </Link>
+                    </li>
+                    )}
+            </ul>
         </div>
     );
 };
