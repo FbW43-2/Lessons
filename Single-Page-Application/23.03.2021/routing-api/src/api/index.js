54a7340fcc1e07afb8c0de68c15c0ce76934e696
Ahmad Osman <ahmad@ahmad-osman.com>
2021-03-25 10:29:16 +0100

    get photos in progress

--- Single-Page-Application/23.03.2021/routing-api/src/api/index.js
+++ Single-Page-Application/23.03.2021/routing-api/src/api/index.js
@@ -87,3 +87,21 @@ export const getAlbums = (id) => {
         })
     })
 }
+
+export const getPhotos = (albumId) => {
+    return new Promise((resolve, reject) => {
+        fetch(`https://jsonplaceholder.typicode.com/albums/${albumId}/photos`).then(response => {
+            if (response.status === 200) {
+                response.json().then(data => {
+                    resolve(data);
+                }).catch(error => {
+                    reject(error);
+                })
+            } else {
+                reject(response.status);
+            }
+        }).catch(error => {
+            reject(error);
+        })
+    })
+}
--- Single-Page-Application/23.03.2021/routing-api/src/components/albums.js
+++ Single-Page-Application/23.03.2021/routing-api/src/components/albums.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { getAlbums } from '../api'
+import { getAlbums, getPhotos } from '../api'
 const Albums = () => {
     const [state, setState] = useState({albums: []});
     const {id} = useParams();
@@ -8,12 +8,28 @@ const Albums = () => {
         getAlbums(id).then(data => {
             setState({albums: data})
         })
-    }, [])
+    }, []);
+
+    const getAlbumPhotos = (albumId) => {
+        getPhotos(albumId).then(data => {
+            const newAlbums = state.albums.map(album => {
+                if (album.id === albumId) {
+                    album.photos = data
+                }
+                return album;
+            });
+            setState({albums: newAlbums})
+
+        })
+    }
+
     return (
         <ul>
             {state.albums.map(album => 
                 <li key={album.id}>
                   <p>  {album.title}  </p>
+                  <button onClick={() => {getAlbumPhotos(album.id)}}>Photos</button>
+                  
                 </li>
             )}
         </ul>
