3fa1dffad9b54a75102cb41a2f220e9433358d80
Ahmad Osman <ahmad@ahmad-osman.com>
2021-03-25 11:06:43 +0100

    get photos done

--- Single-Page-Application/23.03.2021/routing-api/src/components/albums.js
+++ Single-Page-Application/23.03.2021/routing-api/src/components/albums.js
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getAlbums, getPhotos } from '../api'
 const Albums = () => {
-    const [state, setState] = useState({albums: []});
-    const {id} = useParams();
+    const [state, setState] = useState({ albums: [] });
+    const { id } = useParams();
     useEffect(() => {
         getAlbums(id).then(data => {
-            setState({albums: data})
+            setState({ albums: data })
         })
     }, []);
 
@@ -18,20 +18,42 @@ const Albums = () => {
                 }
                 return album;
             });
-            setState({albums: newAlbums})
+            setState({ albums: newAlbums })
 
         })
     }
 
+    const albumsElement = state.albums.map(album => {
+        let photosElement = null;
+        if (album.photos) {
+            photosElement =
+                <div className="row">
+                    {album.photos.map(photo =>
+                        <div key={photo.id} className="col-md-3">
+                            <div class="card" >
+                                <img class="card-img-top"  src={photo.thumbnailUrl} alt={photo.title} />
+                                <div class="card-body">
+                                    <h5 class="card-title">{photo.title}</h5>
+                                    <p class="card-text">{photo.title}</p>
+                                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </div>
+        }
+        return (
+            <li key={album.id}>
+                <p>  {album.title}  </p>
+                <button onClick={() => { getAlbumPhotos(album.id) }}>Photos</button>
+                {photosElement}
+            </li>
+        )
+    });
+
     return (
         <ul>
-            {state.albums.map(album => 
-                <li key={album.id}>
-                  <p>  {album.title}  </p>
-                  <button onClick={() => {getAlbumPhotos(album.id)}}>Photos</button>
-                  
-                </li>
-            )}
+            {albumsElement}
         </ul>
     );
 };
